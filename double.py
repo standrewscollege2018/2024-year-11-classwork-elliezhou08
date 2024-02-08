310ab35ca37f88f05a017abc6df502e9c67b52a2
elliezhou08 <158980210+elliezhou08@users.noreply.github.com>
2024-02-08 15:05:17 +1300

    double with try

--- double.py
+++ double.py
@@ -1,6 +1,12 @@
 ''' take in a num and double it'''
 
-# take input as a float
-coolnum = float(input())
-# print output (i dont like commenting)
+# take input as a float print output (i dont like commenting)
+
+
+
+try:
+    coolnum = float(input("number: "))
+except ValueError:
+    print("i said a NUMBER (idiot)")
+
 print(coolnum*2)
